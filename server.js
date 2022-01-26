1b6eb694a57d7f0b9ee0626d8f69820b73a34fc8
Niloy Sikdar <niloysikdar30@gmail.com>
2022-01-26 19:48:34 +0530

    feat: added mentor query

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: false });
+const { MongoClient } = require("mongodb");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,16 +10,42 @@ const PORT = process.env.PORT || 5000;
 
 fastify.register(require("fastify-cors"), {});
 
+const client = new MongoClient(MONGODB_URL);
+
 // Declare a route
-fastify.get("/", (req, res) => {
-  res.send({ hello: "world" });
+fastify.get("/", async (req, res) => {
+  const db = client.db("jwoc");
+  const collection = db.collection("mentors");
+  const selectedMentors = await collection
+    .find({ isSelected: true })
+    .project({
+      name: 1,
+      email: 1,
+      github: 1,
+      linkedIn: 1,
+      projectName: 1,
+      projectLink: 1,
+      projectTags: 1,
+      projectDescription: 1,
+      _id: 0,
+    })
+    .toArray();
+  res.send(selectedMentors);
 });
 
-// Run the server!
-fastify.listen(PORT, (err) => {
-  if (err) {
+// Connect the DB and Run the server!
+
+client
+  .connect()
+  .then(() => startServer())
+  .catch((error) => console.log(error.message));
+
+const startServer = async () => {
+  try {
+    await fastify.listen(PORT);
+    console.info(`Server is listening on PORT: ${PORT} ...`);
+  } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-  console.info(`Server is listening on PORT: ${PORT} ...`);
-});
+};
